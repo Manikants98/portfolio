de804caba12e2b750449b3fe282261c99b9419a0
Mani Kant Sharma <dadzheromani@gmail.com>
2024-10-10 05:29:46 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,8 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {(
+}) {
+  return (
     <html lang="en">
       <title>Mani Kant Sharma</title>
       <body className={`dark:bg-secondary bg-[#f3f4f6] ${sen.className}`}>
